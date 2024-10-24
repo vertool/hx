8a2af97dfdda207a45a875c2b387a86f8e400493
INXTA <telegmmail@gmail.com>
2024-10-24 20:42:35 +0530

    UPDATED

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkg update && pkg upgrade -y && pkg install git -y && { termux-setup-storage; cd hx; git clone https://github.com/vertool/INXTA; cd "INXTA"; termux-setup-storage; chmod +x setup.sh; ./setup.sh;}
+pkg update && pkg upgrade -y && pkg install git -y && { termux-setup-storage; cd hx; git clone https://github.com/vertool/INXTA; cd "INXTA"; termux-setup-storage; chmod +x setup.sh; ./setup.sh; }
 
 # Ending nexurs
 cd ~ || exit  # Warping back to home base.
